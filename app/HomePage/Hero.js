1403e25f11b3894408388aecb3a2e235c71a373f
Sushanth <sushanthmachavolu@gmail.com>
2024-05-12 13:44:17 +0530

    commit3

--- app/HomePage/Hero.js
+++ app/HomePage/Hero.js
@@ -1,12 +1,17 @@
 import styles from "./Stylesheets/Hero.module.css";
 
 const Hero = () => {
+  //let vh = window.innerHeight;
+  //console.log(vh);
   return (
     <div className={styles.hero}>
       {/* Hero Left */}
       <div className={styles.heroLeft}>
-        <div className={styles.planet}>
+        <div className={styles.planet1}>
           <div className={styles.rocket}>&#x25B2;</div>
+          <div className={styles.crater1}></div>
+          <div className={styles.crater2}></div>
+          <div className={styles.crater3}></div>
         </div>
       </div>
 
